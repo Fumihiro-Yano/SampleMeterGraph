78445aa8eba594f9d1e92247a0972ec7030280e1
矢野史洋 <a2.mj.pop@gmail.com>
2016-05-14 11:52:03 +0900

    add メーターを簡単に描画

--- SampleMeterGraph/ViewController.swift
+++ SampleMeterGraph/ViewController.swift
@@ -9,17 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var graphView:MeterPieGraphView!;
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.
+        var params = [Dictionary<String,AnyObject>]()
+        params.append(["value":5,"color":UIColor.redColor()])
+        params.append(["value":5,"color":UIColor.blueColor()])
+        let width = self.view.frame.width
+        graphView = MeterPieGraphView(frame: CGRectMake(0, 30, width, 320), params: params)
+        self.view.addSubview(graphView)
+        //        graphView.startAnimating()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
